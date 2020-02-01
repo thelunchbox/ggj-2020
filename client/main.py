f8103274e4db0b2cc859eba93b71fb95f559fd49
Donald Bush <donald.bush@crosschx.com>
2020-01-31 22:53:15 -0500

    Basic basics

--- client/main.py
+++ client/main.py
@@ -6,12 +6,30 @@ pygame.display.set_caption("REPAIR GAME")
 screen = pygame.display.set_mode((1500,1020))
 done = False
 
+class Circle():
+    def __init__(self):
+        self.pos = (100,100)
+        self.surface = pygame.Surface((30,30))
+        self.surface.fill((255,100,100))
+    def set_pos(self, pos):
+        self.pos = pos
+    def render(self, screen):
+        screen.blit(self.surface, self.pos)
+
+
+circleObject = Circle()
+
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT or pygame.key.get_pressed()[pygame.K_q]:
             done = True
 
+        if pygame.mouse.get_pressed()[0]:
+            coords = pygame.mouse.get_pos()
+            circleObject.set_pos(coords)
+
     screen.fill((0,0,0))
+    circleObject.render(screen)
     pygame.display.flip()
 
 
