6e337010da5710b20764f0b80c1d471b1ddc058d
Alex Merkert <alex.merkert@gmail.com>
2020-02-01 16:14:55 -0500

    oh yeah

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from rbt.client.client import Client
 from rbt.utils.constants import PORT
 
 if __name__ == '__main__':
-    if len(sys.argv) != 1:
+    print(len(sys.argv))
+    if len(sys.argv) != 2:
         print("Usage:", sys.argv[0], "host")
         print("e.g.", sys.argv[0], "192.168.1.1")
     else:
