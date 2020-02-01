2f0f2d4f5c69baba8600afd51953ab8a8cdfd578
Donald Bush <donald.bush@crosschx.com>
2020-01-31 23:14:59 -0500

    start the game components

---
+++ game_components/test_entities.py
@@ -0,0 +1,11 @@
+
+class Circle():
+    def __init__(self):
+        self.pos = (100,100)
+        self.surface = pygame.Surface((30,30))
+        self.surface.fill((255,100,100))
+    def set_pos(self, pos):
+        self.pos = pos
+    def render(self, screen):
+        screen.blit(self.surface, self.pos)
+
