1a30bb6f8dc34518d64604809936eccf349bdaa7
ssunny <sachinsunny2013@gmail.com>
2020-02-01 17:33:35 -0500

    updated bot.py

--- rbt/game_components/bot.py
+++ rbt/game_components/bot.py
@@ -1,5 +1,38 @@
-#This class represents the robot
-class Bot():
-    def __init__(self, ):
-        self.health = 100
-        self.speed = 1
+import pygame
+import random
+
+
+# This class represents the robots that the player controls
+
+class Bot(pygame.sprite.Sprite):
+
+    def __init__(self, botID, slots, owner):
+        pygame.sprite.Sprite.__init__(self)
+        self.botID = botID
+        self.image = pygame.Surface((30,30))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = 50
+        self.rect.bottom = 50
+        self.slots = slots
+        self.speed = 3
+        self.direction = (0,0)
+        self.ttl = 32000 #TODO: replace with config
+        self.inventory = []
+        self.owner = owner
+
+    def set_pos(self, pos):
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+    def set_color(self, color):
+        self.image.fill(color)
+
+    def move(self):
+        #TODO : write better moving logic
+        if self.owner == 1:
+            self.rect.y+=self.speed
+        else:
+            self.rect.y-= self.speed
+
+    def update(self):
+        self.move()
