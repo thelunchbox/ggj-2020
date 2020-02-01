953e0738c2b89ffc67f5acbe86d4b4a5becccdc9
Michael Peters <peters.cbus@gmail.com>
2020-02-01 13:14:29 -0500

    modify tools

--- rbt/game_components/tools.py
+++ rbt/game_components/tools.py
@@ -1,11 +1,12 @@
 #This class represents the tools
 
-class Tools():
+class Tool():
     #tool ID counter
     idCounter = 0
     
     def __init__(self, tool_type, pos, bot_owned):
-        self.tool_id += 1
+        self.id = Tool.idCounter
+        Tool.idCounter += 1
         self.tool_type = tool_type
         self.pos = pos
         self.bot_owned = bot_owned
