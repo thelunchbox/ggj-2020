9a660c1bf84154c7b9d4f511cbcb6bfe387dd98d
Matt Warren <mattwarren@Matts-MacBook-Pro.local>
2020-02-01 16:36:40 -0500

    made a resistor

--- rbt/game_components/transistor.py
+++ rbt/game_components/transistor.py
@@ -1,13 +1,13 @@
 from rbt.game_components import map_entities
 import pygame
 
-class Material(map_entities.Map_Entities):
+class Transistor(map_entities.Map_Entities):
     def test(self):
         print(1)
     
     # Requires a position, and unique transistor ID       
     def __init__(self, pos, ID):
-        super(Material, self).set_pos(pos, 'transistor')
+        super(Transistor, self).set_pos(pos, 'transistor')
         self.surface = pygame.Surface((30,30))
         self.surface.fill((163,255,15))
         self.ID = ID
