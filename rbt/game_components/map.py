3130bf425bd2f0710afdc8c27e9eeb8621491d9b
Justin <justin2004@hotmail.com>
2020-02-01 17:06:07 -0500

    jank map

--- rbt/game_components/map.py
+++ rbt/game_components/map.py
@@ -1,37 +1,41 @@
-import pygame, os, random
-WIDTH=3 # in tiles
-HEIGHT=3
+import pygame, os, random, copy
+WIDTH=10     # in tiles
+HEIGHT=10
+board = []
+board_rects = []
 
 class Map():
-    def __init__(self):
-        print(os.getcwd())
-        self.tpath = pygame.image.load("rbt/game_components/T_Path.png")
-        self.lpath = pygame.image.load("rbt/game_components/L_Turn.png")
-        self.vpath = pygame.image.load("rbt/game_components/Vertical_Path.png")
-        self.green = pygame.image.load("rbt/game_components/Green_Clear.png")
-        opts = [ self.tpath,self.lpath,self.vpath,self.green ]
 
+    def setBoard(self):
+        self.tile_paths = [ "rbt/game_components/T_Path.png","rbt/game_components/L_Turn.png","rbt/game_components/Vertical_Path.png","rbt/game_components/Green_Clear.png" ]
         self.board       = [[0] * HEIGHT] * WIDTH
         self.board_rects = [[0] * HEIGHT] * WIDTH
         for x in range(WIDTH):
             for y in range(HEIGHT):
-                self.board[x][y] = random.choice(opts)
-                print("board: ", self.board[x][y])
-                self.board_rects[x][y] = self.board[x][y].get_rect()
-                self.board_rects[x][y] = self.board_rects[x][y].move([32 * x, 32 * y])
-                #self.board_rects[x][y] = self.board_rects[x][y].move([x, y])
-                print(x,y)
+                #self.board[x][y] = random.choice(opts)
+                self.board[x][y] = pygame.image.load(random.choice(self.tile_paths))
+                #t0 = pygame.Rect(0,0,32,32).move([32*x,32*y])
+                #self.board_rects[x][y] = t0
+                print("here0 ",hex(id(self.board_rects[x][y])))
 
-
-        self.surface = pygame.Surface((300,300))
-        self.surface.fill((255,100,100))
-    def set_pos(self, pos):
-        self.pos = pos
-    def render(self, screen):
-        #screen.blit(self.tpath,self.tpath_rect)
-        #screen.blit(self.lpath,self.lpath_rect)
         for x in range(WIDTH):
             for y in range(HEIGHT):
-                #print(self.board_rects[x][y])
-                screen.blit(self.board[x][y],self.board_rects[x][y])
+                print("init all rects id ", hex(id(self.board_rects[x][y])))
+
+    def __init__(self):
+        self.surface = pygame.Surface((300,300))
+        self.surface.fill((255,100,100))
+        self.setBoard()
+
+    def set_pos(self, pos):
+        self.pos = pos
+
+    def render(self, screen):
+        print("all rects ", self.board_rects)
+        for x in range(WIDTH):
+            for y in range(HEIGHT):
+                #print(hex(id(self.board_rects[x][y])))
+                print(x,y,self.board_rects[x][y])
+                #screen.blit(self.board[x][y],self.board_rects[x][y])
+                screen.blit(self.board[x][y],[32*x,32*y,32,32])
 
