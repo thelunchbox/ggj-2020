c5b717705bf46c7788953cd756288b5d7330e68e
Donald Bush <donald.bush@crosschx.com>
2020-02-01 19:37:59 -0500

    Screen resolution, map rendering update

--- rbt/game_components/map.py
+++ rbt/game_components/map.py
@@ -25,8 +25,7 @@ class Tile():
 
 class Map():
     def __init__(self):
-        #self.surface = pygame.Surface((300,300))
-        #self.surface.fill((255,100,100))
+        self.surface = pygame.Surface((TILE_WIDTH * MAP_WIDTH, TILE_HEIGHT * MAP_HEIGHT))
         self.setBoard()
 
     def setBoard(self):
@@ -35,9 +34,10 @@ class Map():
             self.board.append([])
             for y in range(MAP_HEIGHT):
                 self.board[x].append(TILE_SURFACES[TILES[x][y]])
+                self.surface.blit(self.board[x][y],(TILE_WIDTH*x,TILE_HEIGHT*y,TILE_WIDTH,TILE_HEIGHT))
 
     def render(self, screen):
         for x in range(MAP_WIDTH):
             for y in range(MAP_HEIGHT):
-                screen.blit(self.board[x][y],(TILE_WIDTH*x,TILE_HEIGHT*y,TILE_WIDTH,TILE_HEIGHT))
+                screen.blit(self.surface, (0,0))
 
