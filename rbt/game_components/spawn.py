7dc439155d3a752012a703c408a596b12c095aa6
Matt Warren <mattwarren@Matts-MacBook-Pro.local>
2020-02-01 22:38:24 -0500

    added a spawn point

---
+++ rbt/game_components/spawn.py
@@ -0,0 +1,28 @@
+from rbt.game_components import map_entities
+import pygame
+
+class Spawn(map_entities.Map_Entities):
+    
+    # Requires a position, unique spawn ID  
+    def __init__(self, pos, ID, value, player):
+        super(Spawn, self).set_pos(pos, 'spawn')
+        self.surface = pygame.Surface((30,30))
+        self.surface.fill((168,30,20))
+        self.ID = ID
+        self.player = player
+        print('New spawn created with ID [' + self.ID + ' at:')
+        print(self.pos[0:2])
+        
+    def render(self, screen):
+        screen.blit(self.surface, self.pos)
+
+    def getState(self):
+        return {
+            'pos': self.pos,
+            'player': self.player
+        }
+
+    def update(self):
+        pass
+        
+        
